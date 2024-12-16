952b84e66e76a57186c277bd956a9a20eab05a91
unknown <korenpeled08@gmail.com>
2024-12-16 22:19:08 +0200

    llll

--- app/error.tsx
+++ app/error.tsx
@@ -1,20 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import Loading from "./Loading";
-
 const Error = () => {
-  const [isClient, setIsClient] = useState(false);
-
-  // This will run only on the client side after the initial render
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient) {
-    return <Loading />;
-  }
-
   return (
     <div className="flex flex-col items-center w-full mt-6">
       <h1 className="text-5xl font-bold mb-4">Something went wrong.</h1>
@@ -24,7 +10,7 @@ const Error = () => {
       </p>
       <button
         className="back-home-btn hover:opacity-85"
-        onClick={() => window.location.reload()} // Safe to call because it's inside useEffect
+        onClick={() => window.location.reload()}
       >
         Try Again
       </button>
