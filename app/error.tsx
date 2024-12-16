f9d47afa4ee90370d0927b085e6dc02adc0c2c5d
unknown <korenpeled08@gmail.com>
2024-12-16 21:53:45 +0200

    d

--- app/error.tsx
+++ app/error.tsx
@@ -1,6 +1,20 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import Loading from "./Loading";
+
 const Error = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  // This will run only on the client side after the initial render
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return <Loading />;
+  }
+
   return (
     <div className="flex flex-col items-center w-full mt-6">
       <h1 className="text-5xl font-bold mb-4">Something went wrong.</h1>
@@ -10,7 +24,7 @@ const Error = () => {
       </p>
       <button
         className="back-home-btn hover:opacity-85"
-        onClick={() => window.location.reload()}
+        onClick={() => window.location.reload()} // Safe to call because it's inside useEffect
       >
         Try Again
       </button>
--- app/pages/jobs/page.tsx
+++ app/pages/jobs/page.tsx
@@ -1,7 +1,7 @@
 import Container from "./Container";
 import { Job } from "../types";
 import { Suspense } from "react";
-import Loading from "./Loading";
+import Loading from "../../Loading";
 
 const Page = async () => {
   console.log("API URL: " + process.env.NEXT_PUBLIC_API_URL);
@@ -11,18 +11,6 @@ const Page = async () => {
   });
 
   const initJobs: Job[] = await response.json();
-
-  // Log the response status and content type to help diagnose issues
-  console.log("Response Status:", response.status);
-  console.log("Response Content-Type:", response.headers.get("Content-Type"));
-
-  // Check if the response is successful and is returning JSON
-  if (!response.ok) {
-    const errorText = await response.text(); // Log the error response body
-    console.error("Failed to fetch jobs, status code:", response.status);
-    console.error("Error body:", errorText);
-    throw new Error("Failed to fetch jobs");
-  }
 
   return (
     <Suspense fallback={<Loading />}>
