fc2464ecc645746e2c089e42389f34fa4ba65f6c
Peled12 <korenpeled08@gmail.com>
2024-12-28 22:27:13 +0200

    title change

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <title>Job Listing</title>
       <body>
         <UserContextProvider>
           <div className="navbar-filler">HI</div>
