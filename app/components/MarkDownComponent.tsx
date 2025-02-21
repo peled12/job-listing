47417b93663bee0a32445d4ffb7f3172f0bb8e2a
Peled12 <korenpeled08@gmail.com>
2025-02-21 17:36:32 +0200

    small change

--- app/components/MarkDownComponent.tsx
+++ app/components/MarkDownComponent.tsx
@@ -1,13 +1,14 @@
 import ReactMarkDown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import remarkBreaks from "remark-breaks";
+import Image from "next/image";
 
 const MarkDownComponent = ({ text }: { text: string | undefined }) => {
   const handleImageRender = (props: { src?: string; alt?: string }) => {
     if (!props.src) return null; // return nothing if the src is empty
 
     return (
-      <img
+      <Image
         src={props.src}
         alt={props.alt || "Image"}
         className="w-[500px] max-h-[250px] object-cover"
