48276ffd07d0089baccf1dd0705d75a6bd0ea64c
Peled12 <korenpeled08@gmail.com>
2025-02-22 16:39:28 +0200

    testing

--- app/pages/jobs/page.tsx
+++ app/pages/jobs/page.tsx
@@ -1,60 +1,30 @@
-"use client";
-
-import { useState, useEffect } from "react";
 import Container from "./Container";
 import { Job } from "../types";
+import { Suspense } from "react";
 import Loading from "../../Loading";
 
-const Page = () => {
-  const [jobs, setJobs] = useState<Job[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
+const fetchJobs = async (): Promise<Job[]> => {
+  console.log(process.env.NEXT_PUBLIC_API_URL);
 
-  // Fetch jobs on component mount
-  useEffect(() => {
-    const fetchJobs = async () => {
-      try {
-        const response = await fetch(
-          process.env.NEXT_PUBLIC_API_URL + "/jobs",
-          {
-            next: { revalidate: 20 }, // revalidate the page every 20 seconds
-          }
-        );
-
-        if (!response.ok) {
-          throw new Error("Failed to fetch jobs");
-        }
-
-        const data = await response.json();
-        setJobs(data); // Set fetched jobs
-      } catch (error) {
-        if (error instanceof Error) {
-          setError(error.message); // Set error message
-        } else {
-          setError("An unknown error occurred");
-        }
-      } finally {
-        setLoading(false); // Hide loading spinner once done
-      }
-    };
-
-    fetchJobs(); // Trigger the fetch function
-  }, []); // Empty dependency array ensures this runs only once when the component mounts
-
-  // Display loading, error, or the jobs data
-  if (loading) {
-    return <Loading />;
+  const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "/jobs", {
+    next: { revalidate: 20 }, // revalidate the page every 20 seconds
+  });
+  if (!response.ok) {
+    throw new Error("Failed to fetch jobs");
   }
 
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
+  return response.json();
+};
+
+const Page = async () => {
+  // fetch jobs
+  const initJobs = await fetchJobs();
 
   return (
-    <div>
+    <Suspense fallback={<Loading />}>
       <h1 className="text-5xl m-8 mb-0">Jobs Listing</h1>
-      <Container initJobs={jobs} />
-    </div>
+      <Container initJobs={initJobs} />
+    </Suspense>
   );
 };
 
